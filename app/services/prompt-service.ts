8573be3b1f39215e3e4f30d4b2daa09cf99854b8
iamdawood@yahoo.com <dawoodahmadmct@gmail.com>
2024-12-08 14:01:24 +0000

    feat: override default sample page with custom form and API service
    - Replaced the default sample page with a custom form featuring a textarea
    - Added a dedicated service for making API calls

---
+++ app/services/prompt-service.ts
@@ -0,0 +1,28 @@
+export const generateStory = async (prompt: string): Promise<String[]> => {
+    try {
+
+        //API URL as per stage local or production
+        const apiURL = window.location.hostname === 'localhoost'
+        ? 'http://localhost:4000/generate'
+        : 'https://storyspark-api.vercel.app/generate';
+
+        const res = await fetch(apiURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({prompt}),
+        });
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch response");
+        }
+
+        const data = await res.json();
+        return data.response || [];
+
+    } catch (error) {
+        console.error(error);
+        throw new Error("Error response");
+    }
+};
