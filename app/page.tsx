a622f2946c83fa7af4dfb18560f31b82db6e3da3
iamdawood@yahoo.com <dawoodahmadmct@gmail.com>
2024-12-09 02:17:04 +0000

    fix: remove unused import mistakenly added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from "next/image";
-import { use, useState } from "react";
+import { useState } from "react";
 import { generateStory, generateStoryImage } from "./services/prompt-service";
 import StoryResponse from "./components/story-response";
 
